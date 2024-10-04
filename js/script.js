c542633b0cd33f7fb059be69a7272996b48787f1
voin-546 <murodd108546@gmail.com>
2024-10-04 12:09:07 +0500

    Ended Desktop version of website

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
     smooth: true,
     tablet: { smooth: true },
-    smartphone: { smotth: true },
-});
+    smartphone: { smooth: true }
+  });
